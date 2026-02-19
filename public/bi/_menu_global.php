0d96a1a7674a8dadc7a834a2ef3408edafced256
Alejandro Olivares <aolivares@adventech-logistica.com>
2026-02-19 09:56:33 -0600

    Add 'Análisis de BL' menu item
    Add a new menu entry 'Análisis de BL' under 'Administración Almacen' in public/bi/_menu_global.php. The item links to catalogos/dashboard_ubicaciones.php and uses the fa-gear icon, exposing the BL analysis dashboard from the warehouse admin menu.

--- public/bi/_menu_global.php
+++ public/bi/_menu_global.php
@@ -529,6 +529,7 @@ require_once __DIR__ . '/../../app/auth_check.php';
             // ADMINISTRACIÓN DE ALMACEN
             'Administración Almacen' => [
                 ['titulo' => 'Config. de Almacén',              'url' => 'config_almacen/configuracion_almacen.php',      'icono' => 'fa-gear'],
+ 		['titulo' => 'Análisis de BL',              'url' => 'catalogos/dashboard_ubicaciones.php',      'icono' => 'fa-gear'],
                 ['titulo' => 'Pallets y Contenedores',          'url' => 'config_almacen/license_plate.php',              'icono' => 'fa-right-left'],
                 ['titulo' => 'Transacciones License Plate',     'url' => 'config_almacen/lp_pr_transaction.php',          'icono' => 'fa-right-left'],
                 ['titulo' => 'Wizard de Ubicaciones',     'url' => 'config_almacen/wizard_ubicaciones.php',          'icono' => 'fa-right-left'],
